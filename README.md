3cbcabf559c6a2ac21dd06d391cb74ae4ec94b49
Jan Sembdner <jan.sembdner@siemens-healthineers.com>
2022-04-02 12:36:17 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftyDataAlgos
+
+A description of this package.
---
+++ Tests/SwiftyDataAlgosTests/SwiftyDataAlgosTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftyDataAlgos
+
+final class SwiftyDataAlgosTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftyDataAlgos().text, "Hello, World!")
+    }
+}
